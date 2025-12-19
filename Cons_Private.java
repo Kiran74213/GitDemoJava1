7931ad9d379b1785f2722504a9b7c645f1fee1f5
kiran <kirandey7076342882@gmail.com>
2025-12-20 00:04:13 +0530

    first commit

---
+++ Cons_Private.java
@@ -0,0 +1,16 @@
+
+
+public class Cons_Private {
+    public static void main(String[] args) {
+       // E re = new E();
+    }
+}
+
+class E{
+    int a; double b; String c;
+    private E()     // Private Constructor ----------------------------------------
+    {
+       a=10; b=10.50; c="Aman";
+       System.out.println(a+" "+b+" "+c); 
+    }
+}
---
+++ Constructor.java
@@ -0,0 +1,25 @@
+
+
+public class Constructor {
+    public static void main(String[] args) {
+
+        B ref = new B();
+        ref.show();
+        
+    }
+}
+
+class B{
+    int a; String name;
+
+    /*B() // Constractor ----------          // If we can not creat any CONSTRUCTOR then java compiler creat
+    {                                        //  a CONSTRUCTOR by itself. **********************************
+        a=1; 
+        name="Rahul";
+    }*/
+
+    void show()   // Method --------------
+    {
+        System.out.println(a+" "+name);
+    }
+}
